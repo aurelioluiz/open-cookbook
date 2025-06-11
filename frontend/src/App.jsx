643a30f1bdb9a8588a34d2baf43952b0af89c1d0
Aurélio Moreira <aurelio.lmoreira@gmail.com>
2025-06-11 16:37:40 -0300

    Add Laravel & React files

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    axios.get('/api/test')
+      .then(response => setData(response.data))
+      .catch(error => console.error(error));
+  }, [])
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        {data && <p>{data.message}</p>}
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
